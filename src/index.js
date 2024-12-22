7b827fd8c9a2d9e7b836e310e0759e66319dd229
rhimack <remackness@outlook.com>
2024-12-22 20:48:40 +0000

    api keys added and js added.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
-function searchSubmit(event) {
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature-value");
+  let temperature = response.data.temperature.current;
+
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "3b3935e5a0ob7af4fft24a1b0a07901a";
+  let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiURL).then(refreshWeather);
+}
+
+function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-input");
-  let cityElement = document.querySelector("#current-city");
-  cityElement.innerHTML = searchInput.value;
+
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
-searchFormElement.addEventListener("submit", searchSubmit);
+searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Paris");
